d90e2be853fc33d74ece87b861fb9ebe3d24ccfd
Umang Kaklotar <umangkaklotar02@gmail.com>
2023-04-25 12:47:15 +0530

    UK || 25/04/2023 || 12:47 PM

--- lib/Helper/ad_helper.dart
+++ lib/Helper/ad_helper.dart
@@ -7,6 +7,8 @@ class AdHelper {
 
   BannerAd? bannerAd;
   InterstitialAd? interstitialAd;
+  AppOpenAd? appOpenAd;
+  RewardedAd? rewardedAd;
 
   loadBannerAds() {
     bannerAd = BannerAd(
@@ -18,15 +20,45 @@ class AdHelper {
 
     bannerAd!.load();
   }
-  
+
   loadInterstitialAds() {
     InterstitialAd.load(
       adUnitId: 'ca-app-pub-3940256099942544/1033173712',
       request: AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) => interstitialAd = ad,
-        onAdFailedToLoad: (error) => print("Erro :: $error"),
+        onAdFailedToLoad: (error) => print("InterstitialAd Erro :: $error"),
       ),
     );
+  }
+
+  loadAppOpenAds() {
+    AppOpenAd.load(
+      adUnitId: 'ca-app-pub-3940256099942544/3419835294',
+      request: AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(
+        onAdLoaded: (ad) {
+          appOpenAd = ad;
+        },
+        onAdFailedToLoad: (error) {
+          print("AppOpenAd Error :: ${error}");
+        },
+      ),
+      orientation: AppOpenAd.orientationPortrait,
+    );
+  }
+
+  loadRewardedAds() {
+    RewardedAd.load(
+        adUnitId: 'ca-app-pub-1162552756272608/8410849545',
+        request: AdRequest(),
+        rewardedAdLoadCallback: RewardedAdLoadCallback(
+          onAdLoaded: (ad) {
+            rewardedAd = ad;
+          },
+          onAdFailedToLoad: (error) {
+            print("RewardedAd Error :: ${error}");
+          },
+        ));
   }
 }
--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -17,7 +17,11 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     AdHelper.adHelper.loadBannerAds();
     AdHelper.adHelper.loadInterstitialAds();
+    AdHelper.adHelper.loadAppOpenAds();
+    AdHelper.adHelper.loadRewardedAds();
   }
+
+  int coin = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +29,41 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("Ads App"),
         centerTitle: true,
+        actions: [
+          Text("$coin"),
+        ],
       ),
       body: Stack(
         children: [
           Center(
-            child: ElevatedButton(
-              child: Text("Interstitial Ads"),
-              onPressed: () async {
-                await AdHelper.adHelper.interstitialAd!.show();
-                AdHelper.adHelper.loadInterstitialAds();
-              },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: const Text("Interstitial Ads"),
+                  onPressed: () async {
+                    await AdHelper.adHelper.interstitialAd!.show();
+                    AdHelper.adHelper.loadInterstitialAds();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text("App Open Ads"),
+                  onPressed: () async {
+                    await AdHelper.adHelper.appOpenAd!.show();
+                    AdHelper.adHelper.loadAppOpenAds();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text("Rewarded Ads"),
+                  onPressed: () async {
+                    await AdHelper.adHelper.rewardedAd!.show(onUserEarnedReward: (view, item) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${item.amount} ${item.type}"),));
+                      coin += item.amount.toInt();
+                    });
+                    AdHelper.adHelper.loadRewardedAds();
+                  },
+                ),
+              ],
             ),
           ),
           Column(
